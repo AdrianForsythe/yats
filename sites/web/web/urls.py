b7d84e0cb4b8d8012deab0fae76fc29e4bff0943
forsythe <adrian.forsythe@ebc.uu.se>
2025-09-18 13:14:06 +0200

    removed caldav integration

--- sites/web/web/urls.py
+++ sites/web/web/urls.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*-
 from django.urls import include, re_path
 from django.contrib import admin
-from django.conf import settings
-from djradicale.views import DjRadicaleView, WellKnownView
 
 admin.autodiscover()
 
@@ -12,14 +9,5 @@ urlpatterns = [
     re_path(r'^', include('yats.check.urls')),
     re_path(r'^', include('yats.urls')),
     re_path(r'^admin/', admin.site.urls),
-
-    re_path(r'^' + settings.DJRADICALE_CONFIG['server']['base_prefix'].lstrip('/'), include(('djradicale.urls', 'djradicale'))),
-
-    # .well-known external implementation
-    re_path(r'^\.well-known/(?P<type>(caldav|carddav))$',
-        WellKnownView.as_view(), name='djradicale_well-known'),
-
-    # .well-known internal (radicale) implementation
-    re_path(r'^\.well-known/(?P<type>(caldav|carddav))$',
-         DjRadicaleView.as_view(), name='djradicale_well-known'),
+    # No CalDAV URLs
 ]
