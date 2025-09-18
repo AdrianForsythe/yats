616f0983ab0118fd2d3432cd07aff4a0598387ca
forsythe <adrian.forsythe@ebc.uu.se>
2025-09-18 14:13:00 +0200

    added missing org middleware

--- sites/web/web/settings.py
+++ sites/web/web/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'yats.middleware.auth.OrgaAdditionMiddleware',  # Add this
 ]
 
 ROOT_URLCONF = 'web.urls'
